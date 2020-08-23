66aa6f7a06fda6d6c49ba47ba2606d285e1ac260
Robert Vo <>
2020-08-23 12:42:41 +0700

    Updates

---
+++ precache-manifest.0f69ba324933a2e1035270ed4951e311.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2441e87ec337bf560c2a7cec3c7c220",
+    "url": "/album-client/index.html"
+  },
+  {
+    "revision": "35d59f20627fa461a933",
+    "url": "/album-client/static/css/main.1d6893ed.chunk.css"
+  },
+  {
+    "revision": "bdba3d90c972df2ac2e2",
+    "url": "/album-client/static/js/2.fa18e287.chunk.js"
+  },
+  {
+    "revision": "b0382a1d868abef989ec0625e2034248",
+    "url": "/album-client/static/js/2.fa18e287.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35d59f20627fa461a933",
+    "url": "/album-client/static/js/main.94f5c16a.chunk.js"
+  },
+  {
+    "revision": "913142731107c2b32a0f",
+    "url": "/album-client/static/js/runtime-main.92db1bd5.js"
+  }
+]);
